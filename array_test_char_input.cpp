95e3c996c43efe6fa7157189c5d8d15ff20dcf05
jeaimehp <jeaimehp@gmail.com>
2018-02-08 12:38:25 -0600

    spelling correction

--- array_test_char_input.cpp
+++ array_test_char_input.cpp
@@ -13,7 +13,7 @@ using std::endl;
 int main () {
 	char array_alphabets [26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 	int number_element;
-	cout << "What element number woudl you liek to display?" << endl;
+	cout << "What element number would you like to display?" << endl;
 	cin >> number_element;
 	cout << "Element "<< number_element << " of array_alphabet is " << array_alphabets [number_element] << endl;
 
